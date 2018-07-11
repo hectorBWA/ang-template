90c6a8518c2996b59394d4d4846bee7210e0e6cf
hectorBWA <hectorzavala12345@gmail.com>
2018-07-11 14:09:12 -0700

    committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showSummer: boolean;
+  showWinter: boolean;
+  showFall: boolean;
+  showLogo: boolean;
+    constructor() {
+        this.showSummer = false;
+        this.showWinter = false;
+        this.showFall = false;
+        this.showLogo = true;
+    }
+    ShowSummerButton() {
+        this.showSummer = true;
+        this.showWinter = false;
+        this.showFall = false;
+        this.showLogo = false;
+    }
+    ShowWinterButton() {
+      this.showWinter = true;
+      this.showSummer = false;
+        this.showFall = false;
+        this.showLogo = false;
+  }
+  ShowFallButton() {
+    this.showFall = true;
+    this.showWinter = false;
+        this.showSummer = false;
+        this.showLogo = false;
+}
+    RefreshButton() {
+      this.showSummer = false;
+      this.showWinter = false;
+      this.showFall = false;
+      this.showLogo = true;
+    }
+     on() {
+      document.getElementById("overlay").style.display = "block";
+  }
+  
+  off() {
+      document.getElementById("overlay").style.display = "none";
+  }
 }
